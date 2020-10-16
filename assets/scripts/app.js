e848c6c16c3f91a8287cb3cdc4c13b9601837870
Alexander Bos <abos@revridge.com>
2020-10-15 20:33:02 -0400

    Sign in --> Sign Out commit
    modifies apps, store, index.html

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
 'use strict'
-
+const events = require('./auth/events')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -7,5 +7,10 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
-})
+  $('#change-password-user-form').hide(),
+  $('#sign-up-user-form').on('submit', events.onSignUp),
+  $('#sign-in-user-form').on('submit', events.onSignIn),
+  $('#change-password-user-form').on('submit', events.onChangePassword),
+  $('#sign-out-user-form').on('submit', events.onSignOut)
+}
+)
--- assets/scripts/store.js
+++ assets/scripts/store.js
@@ -1,6 +1,11 @@
 'use strict'
 
 const store = {
+  user: 'credentials[email]',
+  password: 'credentials[password]',
+  authorization: 'authorization[TOKEN]',
+  oldpw: 'passwords[oldpw]',
+  newpw: 'passwords[newpw]'
 }
 
 module.exports = store
